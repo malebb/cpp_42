751e36cbeee0a444ff9702c30511262769e1b07d
mlebrun <mlebrun@student.42.fr>
2021-06-10 22:06:38 +0200

    feat: magaphone program

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int	main(int argc, char **argv)
+{
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+
+	for (int i = 1; i < argc; i++)
+	{
+		for (int j = 0; j < (int)strlen(argv[i]); j++)
+		{
+			std::cout << (char)toupper(argv[i][j]);
+		}
+	}
+	std::cout << endl;
+	return 0;
+}
