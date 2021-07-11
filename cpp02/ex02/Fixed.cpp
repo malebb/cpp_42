444580feffce2a3e157f281a3c25db0ccd3f2e10
mlebrun <mlebrun@student.42.fr>
2021-07-11 16:22:12 +0200

    feat: create class with more op

---
+++ cpp02/ex02/Fixed.cpp
@@ -0,0 +1,85 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlebrun <mlebrun@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/06 14:44:31 by mlebrun           #+#    #+#             */
+/*   Updated: 2021/07/11 16:17:03 by mlebrun          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+Fixed::Fixed() : _fixed_point(0)
+{
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+Fixed::Fixed(const int nb) : _fixed_point(nb * 256)
+{
+
+	std::cout << "Int constructor called" << std::endl;
+}
+
+Fixed::Fixed(const float nb) : _fixed_point((int)std::roundf(nb * 256))
+{
+	std::cout << "Float constructor called" << std::endl;
+}
+
+float	Fixed::toFloat( void ) const
+{
+	float		fixed_point;
+
+	fixed_point = (float)this->_fixed_point;
+	return ((float)(fixed_point / 256));
+}
+
+int		Fixed::toInt( void ) const
+{
+	return ((int)this->_fixed_point / 256);
+}
+
+Fixed::Fixed( Fixed const & src )
+{
+	std::cout << "Copy constructor called" << std::endl;
+	*this = src;
+	return ;
+}
+
+Fixed&		Fixed::operator=( Fixed const & rhs )
+{
+	std::cout << "Assignation operator called" << std::endl;
+	this->_fixed_point = rhs._fixed_point;
+	return *this;
+}
+
+std::ostream & operator<<(std::ostream & o, Fixed const & rhs)
+{
+	o << rhs.toFloat();
+	return o;
+}
+
+int	operator>(Fixed const & rhs )
+{
+	if (this->_fixed_point > rhs._fixed_point)
+		return (1);
+	return (0);
+}
+
+int		Fixed::getRawBits( void ) const
+{
+	std::cout << "getRawbits member function called" << std::endl;
+	return (this->_fixed_point);
+}
+
+void	Fixed::setRawBits( int const raw )
+{
+	this->_fixed_point = raw;
+}
---
+++ cpp02/ex02/Fixed.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlebrun <mlebrun@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/06 14:42:39 by mlebrun           #+#    #+#             */
+/*   Updated: 2021/07/11 16:20:53 by mlebrun          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cmath>
+
+class Fixed
+{
+	public :
+		Fixed();
+		Fixed( Fixed const & src );
+		Fixed(const float nb);
+		Fixed(const int nb);
+		Fixed&	operator=( Fixed const & rhs );
+		int		operator>( Fixed const & rhs );
+		int		getRawBits( void ) const;
+		void	setRawBits( int const raw );
+		float	toFloat( void ) const;
+		int		toInt( void ) const;
+		~Fixed();
+
+	private :
+		int					_fixed_point;
+		static const int	_fract_bit_nb = 8;
+};
+
+std::ostream & operator<<( std::ostream & o, Fixed const & rhs );
