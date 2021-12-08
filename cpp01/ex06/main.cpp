35741e90ec0f0dc8ea654b5c5424edb7ecf54f9c
mlebrun <mlebrun@student.42.fr>
2021-12-08 15:41:49 +0100

    start implement switch

--- cpp01/ex06/main.cpp
+++ cpp01/ex06/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mlebrun <mlebrun@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/06 11:01:04 by mlebrun           #+#    #+#             */
-/*   Updated: 2021/07/06 11:28:59 by mlebrun          ###   ########.fr       */
+/*   Updated: 2021/12/08 15:36:24 by mlebrun          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,29 +14,31 @@
 
 int		main(int argc, char **argv)
 {
-	std::string		levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
-	Karen			karen;
-	int				found;
+	std::string		level;
+	std::string		log_levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	unsigned int	i;
 
 	if (argc != 2)
-		return (1);
-	std::string 	level(argv[1]);
-	found = 0;
-	for (int i = 0; i < 4; i++)
+		std::cout << "error: only expect one log level" << std::endl;
+
+	//	std::cout << "Error: only expect one of these log level: DEGUB INFO WARNING ERROR\n";
+	level = argv[1];
+	getline(std::cin, level);
+	switch (level)
 	{
-		if (!level.compare(levels[i]))
-		{
-			for (int j = i; j < 4; j++)
-			{
-				std::cout << "[" << levels[j] << "]" << std::endl;
-				karen.complain(levels[j]);
-				if (j != 3)
-					std::cout << std::endl;
-			}
-			found = 1;
-		}
+		case "DEBUG": 
+			i = 0;
+			break ;
+		case "INFO":
+			i = 1;
+			break ;
+		case "WARNING":
+			i = 2;
+			break ;
+		case "ERROR":
+			i = 3;
+			break ;
 	}
-	if (!found)
-		std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+	std::cout << "result = " << level << std::endl;
 	return (0);
 }
