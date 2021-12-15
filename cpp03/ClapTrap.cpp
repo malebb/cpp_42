f1b09b6cbe1e7cea242a3cef92e895e3c6d3aa99
mlebrun <mlebrun@student.42.fr>
2021-12-15 13:49:49 +0100

    ex00 done

--- cpp03/ClapTrap.cpp
+++ cpp03/ClapTrap.cpp
@@ -1,24 +1,45 @@
-ClapTrap::ClapTrap(std::string name) : _name(name), _hit_point(10), _energy_point(10) _attack_damage(0)
+#include "ClapTrap.hpp"
+
+ClapTrap::ClapTrap(std::string name) : _name(name), _hit_points(10), _energy_points(10), _attack_damage(0)
 {
-	std::cout << "oh! it's " << name << "!" <<std::endl;
+	std::cout << "Oh ! it's " << name << " ! HP = " << this->_hit_points << std::endl;
 }
 
 ClapTrap::~ClapTrap()
 {
-	std::cout << "bye " << name << std::endl;
+	std::cout << "Bye " << this->_name << std::endl;
 }
 
 void	ClapTrap::attack(std::string const & target)
 {
-	std::cout << "ClapTrap " << this->name << " attack " << target std::endl;
+	if (this->_energy_points >= 2)
+	{
+		std::cout << "ClapTrap " << this->_name << " attack " \
+			<< target << ", causing " << this->_attack_damage << " points of damage" << std::endl;
+		this->_energy_points -= 2;
+	}
+	else
+		std::cout << "ClapTrap " << this->_name << " is out of energy" << std::endl;
 }
 
 void	ClapTrap::takeDamage(unsigned int amount)
 {
-
+	if (this->_hit_points > 0)
+	{
+		this->_hit_points -= amount;
+		std::cout << "ClapTrap " << this->_name << " has been attacked ! -" \
+		<< amount << " HP" << std::endl;
+		if (this->_hit_points <= 0)
+			std::cout << "ClapTrap " << this->_name << " is out of order" << std::endl;
+	}
+	else
+		std::cout << "ClapTrap " << this->_name << " is already out of order" << std::endl;
+		
 }
 
 void	ClapTrap::beRepaired(unsigned int amount)
 {
-	
+	this->_hit_points += amount;
+	std::cout << "ClapTrap " << this->_name << "has been repaired ! +" \
+	<< amount << " HP"<< std::endl;
 }
--- cpp03/ClapTrap.hpp
+++ cpp03/ClapTrap.hpp
@@ -1,15 +1,22 @@
-#include "ClapTrap.hpp"
+#ifndef		CLAPTRAP_H
+#define		CLAPTRAP_H
+
+#include <iostream>
 
 class ClapTrap
 {
 	public :
+		ClapTrap(std::string name);
+		~ClapTrap();
 		void			attack(std::string const & target);
 		void			takeDamage(unsigned int amount);
 		void			beRepaired(unsigned int amount);
 
 	private :
-		std::string		name;
-		unsigned int	hit_points;
-		unsigned int	energy_point;
-		unsigned int	attack_damage;
+		std::string		_name;
+		unsigned int	_hit_points;
+		unsigned int	_energy_points;
+		unsigned int	_attack_damage;
 };
+
+#endif
