8a6ec491e8b24e7b0276fb0d8717b1cac2fb9a94
mlebrun <mlebrun@student.42.fr>
2021-12-23 09:50:44 +0100

    create main for ex02

--- cpp04/ex02/main.cpp
+++ cpp04/ex02/main.cpp
@@ -6,18 +6,45 @@
 
 int	main(void)
 {
-	const Cat		*cat = new Cat();
-	const AAnimal*		animal;
+	Brain			*brain;
 
-	animal = cat;
+	std::cout << "------------Dog test------------" <<std::endl;
+	std::cout << std::endl;
+
+	const Dog		*dog = new Dog();
 
 	std::cout << std::endl;
 
-	std::cout << "First animal has type " << cat->getType() << std::endl;
+	brain = dog->get_brain();
+	brain->ideas[0] = "Where is my boneee??";
+	std::cout << dog->getType() << " is thinking : "
+		<< brain->ideas[0] << std::endl;
+	dog->makeSound();
+
+	std::cout << std::endl;
+
+	delete dog;
+
+	std::cout << std::endl;
+
+	std::cout << "------------Cat test------------" <<std::endl;
+	std::cout << std::endl;
+
+	const Cat *cat = new Cat();
+
+	std::cout << std::endl;
+
+	brain = cat->get_brain();
+	brain->ideas[0] = "Where is my mouseee??";
+	std::cout << cat->getType() << " is thinking : "
+		<< brain->ideas[0] << std::endl;
 	cat->makeSound();
+
 	std::cout << std::endl;
 
-	delete cat;
+	delete dog;
+
+//	const AAnimal animal = AAnimal(); //Error because AAnimal = abstract
 
 	return (0);
 }
